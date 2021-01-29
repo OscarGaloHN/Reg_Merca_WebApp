eb67d4450fd258e7a03ec55dc9a625d47028b737
OscarGaloHN <oscaramador7@gmail.com>
2021-01-28 19:45:53 -0600

    Funciones de Conexion y Consulta

--- Reg_Merca_WebApp/modulos/src/ControlDB.vb
+++ Reg_Merca_WebApp/modulos/src/ControlDB.vb
@@ -1,3 +1,57 @@
-﻿Public Class ControlDB
+﻿Imports MySql.Data.MySqlClient
 
+Public Class ControlDB
+    Implements IDisposable
+    Public Enum TipoConexion
+        CONEXION_BASE_USUARIO = 1
+
+    End Enum
+    Private Function GetCadenaConexion(ByRef Tipo As TipoConexion) As String
+        Select Case Tipo
+            Case TipoConexion.CONEXION_BASE_USUARIO
+                Return ConfigurationManager.ConnectionStrings("Cstr_1").ConnectionString
+        End Select
+        Return String.Empty
+    End Function
+
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        GC.SuppressFinalize(Me)
+    End Sub
+
+    Function GME(ByRef Ssql As String, ByRef Tipo As TipoConexion)
+        Using con As New MySqlConnection(GetCadenaConexion(Tipo))
+            con.Open()
+            Dim command As MySqlCommand
+            Dim Adapter As MySqlDataAdapter = New MySqlDataAdapter()
+            command = New MySqlCommand(Ssql, con)
+            Adapter.InsertCommand = New MySqlCommand(Ssql, con)
+            Adapter.InsertCommand.ExecuteNonQuery()
+            command.Dispose()
+            con.Close()
+        End Using
+        'Dim conn As String = ConfigurationManager.ConnectionStrings("Cstr_6").ConnectionString
+        'Dim con As SqlConnection = New SqlConnection(conn)
+        Return Ssql
+    End Function
+
+
+    Function SelectX(ByRef Ssql As String, ByRef Tipo As TipoConexion)
+        Dim ds As DataSet = New DataSet()
+
+        Using con As New MySqlConnection(GetCadenaConexion(Tipo))
+            con.Open()
+
+            Dim cmd As MySqlCommand = New MySqlCommand(Ssql, con)
+            cmd.CommandType = CommandType.Text
+            Dim sda As MySqlDataAdapter = New MySqlDataAdapter(cmd)
+            sda.Fill(ds)
+            cmd.Dispose()
+            'Dim NumReg As Integer = ds.Tables(0).Rows.Count
+
+        End Using
+        Return ds
+
+    End Function
+    '*****************
 End Class
