3c721c3cc95c0870c23832bc7b703bf8154822ca
OscarGaloHN <oscaramador7@gmail.com>
2021-01-28 19:54:27 -0600

    cadena de coneccion

--- Reg_Merca_WebApp/modulos/src/ControlDB.vb
+++ Reg_Merca_WebApp/modulos/src/ControlDB.vb
@@ -3,22 +3,18 @@
 Public Class ControlDB
     Implements IDisposable
     Public Enum TipoConexion
-        CONEXION_BASE_USUARIO = 1
-
+        Cx_Aduana = 1
     End Enum
     Private Function GetCadenaConexion(ByRef Tipo As TipoConexion) As String
         Select Case Tipo
-            Case TipoConexion.CONEXION_BASE_USUARIO
+            Case TipoConexion.Cx_Aduana
                 Return ConfigurationManager.ConnectionStrings("Cstr_1").ConnectionString
         End Select
         Return String.Empty
     End Function
-
-
     Public Sub Dispose() Implements IDisposable.Dispose
         GC.SuppressFinalize(Me)
     End Sub
-
     Function GME(ByRef Ssql As String, ByRef Tipo As TipoConexion)
         Using con As New MySqlConnection(GetCadenaConexion(Tipo))
             con.Open()
@@ -34,24 +30,18 @@ Public Class ControlDB
         'Dim con As SqlConnection = New SqlConnection(conn)
         Return Ssql
     End Function
-
-
     Function SelectX(ByRef Ssql As String, ByRef Tipo As TipoConexion)
         Dim ds As DataSet = New DataSet()
-
         Using con As New MySqlConnection(GetCadenaConexion(Tipo))
             con.Open()
-
             Dim cmd As MySqlCommand = New MySqlCommand(Ssql, con)
             cmd.CommandType = CommandType.Text
             Dim sda As MySqlDataAdapter = New MySqlDataAdapter(cmd)
             sda.Fill(ds)
             cmd.Dispose()
             'Dim NumReg As Integer = ds.Tables(0).Rows.Count
-
         End Using
         Return ds
-
     End Function
     '*****************
 End Class
