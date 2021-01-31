08a1193f02d70c0b2d98667d38221bf9b5481ef9
OscarGaloHN <oscaramador7@gmail.com>
2021-01-31 12:59:46 -0600

    Diseño completo login

--- Reg_Merca_WebApp/Inicio/jsLogin.js
+++ Reg_Merca_WebApp/Inicio/jsLogin.js
@@ -1,4 +1,5 @@
-﻿$(document).ready(function () {
+﻿//mostrar contraseña
+$(document).ready(function () {
     $('#show_password').hover(function show() {
         //Cambiar el atributo a texto
         $('#txtContra').attr('type', 'text');
@@ -12,3 +13,15 @@
             //$('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
         });
 });
+
+//SOLO NUMERO Y LETRAS
+function isNumberOrLetter(evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    if ((charCode >= 65 && charCode < 91) || (charCode >= 97 && charCode < 123) || (charCode > 47 && charCode < 58) || (charCode = 241)  || ( charCode = 209))
+        return true;
+    return false;
+}
+//pasar a mayusculas
+function mayus(e) {
+    e.value = e.value.toUpperCase();
+}
