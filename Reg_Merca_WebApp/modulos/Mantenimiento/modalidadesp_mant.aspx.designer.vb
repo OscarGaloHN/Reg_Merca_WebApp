21f77d955a2df36e0ef0341b9e08c5686cdde812
mel-isab <melany.hernandez@unah.hn>
2021-03-23 14:12:44 -0600

    cambios

---
+++ Reg_Merca_WebApp/modulos/Mantenimiento/modalidadesp_mant.aspx.designer.vb
@@ -0,0 +1,132 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class modalidadesp_mant
+
+    '''<summary>
+    '''Control gvCustomers.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents gvCustomers As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Control Panel3.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Panel3 As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Control txtnombremodalidad.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtnombremodalidad As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control reqnombrevacio.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents reqnombrevacio As Global.System.Web.UI.WebControls.RequiredFieldValidator
+
+    '''<summary>
+    '''Control bttGuardarModalidad.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents bttGuardarModalidad As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control lblAduna.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblAduna As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control lblHiddenIDMdalidad.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblHiddenIDMdalidad As Global.System.Web.UI.WebControls.HiddenField
+
+    '''<summary>
+    '''Control lblHiddenNombreModalidad.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblHiddenNombreModalidad As Global.System.Web.UI.WebControls.HiddenField
+
+    '''<summary>
+    '''Control bttEliminarModalidad.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents bttEliminarModalidad As Global.System.Web.UI.WebControls.LinkButton
+
+    '''<summary>
+    '''Control Panel1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Panel1 As Global.System.Web.UI.WebControls.Panel
+
+    '''<summary>
+    '''Control txtnombremodalidadEditar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtnombremodalidadEditar As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control RequiredFieldValidator3.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents RequiredFieldValidator3 As Global.System.Web.UI.WebControls.RequiredFieldValidator
+
+    '''<summary>
+    '''Control bttModificar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents bttModificar As Global.System.Web.UI.WebControls.LinkButton
+End Class
---
+++ Reg_Merca_WebApp/modulos/Mantenimiento/modalidadesp_mant.aspx.vb
@@ -0,0 +1,127 @@
+﻿Public Class modalidadesp_mant
+    Inherits System.Web.UI.Page
+    Private Property DataSetX As DataSet
+        Get
+            Return CType(Session("DataSetX"), DataSet)
+        End Get
+        Set(ByVal value As DataSet)
+            Session("DataSetX") = value
+        End Set
+    End Property
+    'OBJETO #17
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Try
+            'llenar grid
+            Dim Ssql As String = String.Empty
+            Ssql = "SELECT * FROM DB_Nac_tbl_39_modalidad_especial"
+            Using con As New ControlDB
+                DataSetX = con.SelectX(Ssql, ControlDB.TipoConexion.Cx_Aduana)
+                Session("NumReg") = DataSetX.Tables(0).Rows.Count
+            End Using
+            If Session("NumReg") > 0 Then
+                gvCustomers.DataSource = DataSetX
+                gvCustomers.DataBind()
+            End If
+
+            If Not IsPostBack Then
+                Select Case Request.QueryString("acction")
+                    Case "newaduana"
+                        Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','La aduana se almaceno con exito.', 'success');</script>")
+                    Case "delteaduana"
+                        Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','La aduana se elimino con exito.', 'success');</script>")
+                    Case "editduana"
+                        Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','La aduana se modifico con exito.', 'success');</script>")
+                    Case Else
+                        'bitacora de que salio de un form
+                        If Not IsPostBack Then
+                            Using log_bitacora As New ControlBitacora
+                                log_bitacora.acciones_Comunes(10, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "El usuario sale a la pantalla de " & Session("NombrefrmQueIngresa"))
+                            End Using
+                        End If
+
+                        'bitacora de que ingreso al form
+                        Session("IDfrmQueIngresa") = 17
+                        Session("NombrefrmQueIngresa") = "Mantenimiento de Aduanas"
+                        If Not IsPostBack Then
+                            Using log_bitacora As New ControlBitacora
+                                log_bitacora.acciones_Comunes(9, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "El usuario ingresa a la pantalla de " & Session("NombrefrmQueIngresa"))
+                            End Using
+                        End If
+                End Select
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub bttGuardarAduana_Click(sender As Object, e As EventArgs) Handles bttGuardarAduana.Click
+        Try
+            Dim Ssql As String = String.Empty
+            Ssql = "SELECT * FROM DB_Nac_Merca.tbl_06_aduanas where Nombre_aduana = BINARY  '" & txtAduana.Text & "' "
+            Using con As New ControlDB
+                DataSetX = con.SelectX(Ssql, ControlDB.TipoConexion.Cx_Aduana)
+                Session("NumReg") = DataSetX.Tables(0).Rows.Count
+            End Using
+            If Session("NumReg") > 0 Then
+                Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','El nombre de aduana ya esta registrado.', 'error');</script>")
+            Else
+                Ssql = "INSERT INTO `DB_Nac_Merca`.`tbl_06_aduanas` (`Nombre_aduana`, `Contacto`, `Tel`, `Ubicacion`) VALUES ('" & txtAduana.Text & "', '" & txtContacto.Text & "', '" & txtTel.Text & "', '" & txtDireccion.Text & "');"
+                Using con As New ControlDB
+                    con.GME(Ssql, ControlDB.TipoConexion.Cx_Aduana)
+                End Using
+                Using log_bitacora As New ControlBitacora
+                    log_bitacora.acciones_Comunes(4, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "Se editaron los datos para la aduana con id: " & lblHiddenIDAduna.Value)
+                End Using
+                Response.Redirect("~/modulos/mantenimiento/mantenimiento_adunas.aspx?acction=editduana")
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub bttEliminarAduna_Click(sender As Object, e As EventArgs) Handles bttEliminarAduna.Click
+        Try
+            Dim Ssql As String = "DELETE FROM `DB_Nac_Merca`.`tbl_06_aduanas` WHERE Id_Aduana= " & lblHiddenIDAduna.Value
+            Using con As New ControlDB
+                con.GME(Ssql, ControlDB.TipoConexion.Cx_Aduana)
+            End Using
+            Using log_bitacora As New ControlBitacora
+                log_bitacora.acciones_Comunes(6, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "Se elimino la aduna con nombre: " & lblHiddenNombreAduna.Value & " con exito")
+            End Using
+            Response.Redirect("~/modulos/mantenimiento/mantenimiento_adunas.aspx?acction=delteaduana")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub bttModificar_Click(sender As Object, e As EventArgs) Handles bttModificar.Click
+        Try
+            Dim Ssql As String = String.Empty
+            If txtAduanaEditar.Text <> lblHiddenNombreAduna.Value Then
+                Ssql = "SELECT * FROM DB_Nac_Merca.tbl_06_aduanas where Nombre_aduana = BINARY  '" & txtAduanaEditar.Text & "' "
+                Using con As New ControlDB
+                    DataSetX = con.SelectX(Ssql, ControlDB.TipoConexion.Cx_Aduana)
+                    Session("NumReg") = DataSetX.Tables(0).Rows.Count
+                End Using
+            Else
+                Session("NumReg") = 0
+            End If
+
+            If Session("NumReg") > 0 Then
+                Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','El nombre de aduana ya esta registrado.', 'error');</script>")
+            Else
+                Ssql = "UPDATE `DB_Nac_Merca`.`tbl_06_aduanas` SET `Nombre_aduana` = '" & txtAduanaEditar.Text & "', `Contacto` = '" & txtContactoEditar.Text & "', `Tel` = " & txtTelEditar.Text & ", `Ubicacion` = '" & txtDireccionEditar.Text & "' WHERE `Id_Aduana` = " & lblHiddenIDAduna.Value & ";"
+                Using con As New ControlDB
+                    con.GME(Ssql, ControlDB.TipoConexion.Cx_Aduana)
+                End Using
+                Using log_bitacora As New ControlBitacora
+                    log_bitacora.acciones_Comunes(4, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "Se guardo una nueva aduna con nombre: " & txtAduana.Text)
+                End Using
+                Response.Redirect("~/modulos/mantenimiento/mantenimiento_adunas.aspx?acction=newaduana")
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+End Class
