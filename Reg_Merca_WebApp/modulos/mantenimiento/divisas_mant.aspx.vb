ba1c6c4782f4bd13db7177f6ab036cd2ba3fcb0d
mel-isab <melany.hernandez@unah.hn>
2021-04-01 14:50:49 -0600

    cambios

--- Reg_Merca_WebApp/modulos/mantenimiento/divisas_mant.aspx.vb
+++ Reg_Merca_WebApp/modulos/mantenimiento/divisas_mant.aspx.vb
@@ -27,11 +27,11 @@
             If Not IsPostBack Then
                 Select Case Request.QueryString("acction")
                     Case "newdivisas"
-                        Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','La aduana se almaceno con exito.', 'success');</script>")
+                        Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Divisas','La divisa se almaceno con exito.', 'success');</script>")
                     Case "deltedivisas"
-                        Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','La aduana se elimino con exito.', 'success');</script>")
+                        Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Divisas','La divisa se elimino con exito.', 'success');</script>")
                     Case "editdivisas"
-                        Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','La aduana se modifico con exito.', 'success');</script>")
+                        Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Divisas','La divisa se modifico con exito.', 'success');</script>")
                     Case Else
                         'bitacora de que salio de un form
                         If Not IsPostBack Then
@@ -63,7 +63,7 @@
         Try
             Dim Ssql As String = String.Empty
             If txtdescripcionEditar.Text <> lblHiddenNombreDivisas.Value Then
-                Ssql = "SELECT * FROM DB_Nac_Merca.tbl_29_Divisas where Nombre_aduana = BINARY  '" & txtdescripcionEditar.Text & "' "
+                Ssql = "SELECT * FROM DB_Nac_Merca.tbl_29_Divisas where Descripcion = BINARY  '" & txtdescripcionEditar.Text & "' "
                 Using con As New ControlDB
                     DataSetX = con.SelectX(Ssql, ControlDB.TipoConexion.Cx_Aduana)
                     Session("NumReg") = DataSetX.Tables(0).Rows.Count
@@ -73,16 +73,16 @@
             End If
 
             If Session("NumReg") > 0 Then
-                Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','El nombre de aduana ya esta registrado.', 'error');</script>")
+                Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Divisas','La Divisa ya esta registrado.', 'error');</script>")
             Else
-                Ssql = "UPDATE `DB_Nac_Merca`.`tbl_06_aduanas` SET `Descripcion` = '" & txtdescripcionEditar.Text & "', `Total_Factura` = '" & txttotalfacturaEditar.Text & "', `Total_Flete` = " & txttotalfleteEditar.Text & ", `Total_Seguro` = '" & txttotalseguroEditar.Text & "', `Total_Otros_gastos` = '" & txttotalotrosEditar.Text & "' WHERE `Id_Divisas` = " & lblHiddenIDdivisas.Value & ";"
+                Ssql = "UPDATE `DB_Nac_Merca`.`tbl_29_Divisas` SET `Descripcion` = '" & txtdescripcionEditar.Text & "', `Total_Factura` = '" & txttotalfacturaEditar.Text & "', `Total_Flete` = " & txttotalfleteEditar.Text & ", `Total_Seguro` = '" & txttotalseguroEditar.Text & "', `Total_Otros_gastos` = '" & txttotalotrosEditar.Text & "' WHERE `Id_Divisas` = " & lblHiddenIDdivisas.Value & ";"
                 Using con As New ControlDB
                     con.GME(Ssql, ControlDB.TipoConexion.Cx_Aduana)
                 End Using
                 Using log_bitacora As New ControlBitacora
-                    log_bitacora.acciones_Comunes(4, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "Se guardo una nueva aduna con nombre: " & txtdescripcion.Text)
+                    log_bitacora.acciones_Comunes(4, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "Se guardo una nueva divisa con nombre: " & txtdescripcion.Text)
                 End Using
-                Response.Redirect("~/modulos/mantenimiento/mantenimiento_adunas.aspx?acction=newaduana")
+                Response.Redirect("~/modulos/mantenimiento/divisas_mant.aspx?acction=newdivisas")
             End If
         Catch ex As Exception
 
@@ -92,22 +92,22 @@
     Private Sub bttGuardarDivisa_Click(sender As Object, e As EventArgs) Handles bttGuardarDivisa.Click
         Try
             Dim Ssql As String = String.Empty
-            Ssql = "SELECT * FROM DB_Nac_Merca.tbl_29_Divisas where Descripcion = BINARY  '" & txtdescripcion.Text & "' "
+            Ssql = "SELECT * FROM DB_Nac_Merca.tbl_29_Divisas where Descripcion = '" & txtdescripcion.Text & "' "
             Using con As New ControlDB
                 DataSetX = con.SelectX(Ssql, ControlDB.TipoConexion.Cx_Aduana)
                 Session("NumReg") = DataSetX.Tables(0).Rows.Count
             End Using
             If Session("NumReg") > 0 Then
-                Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Aduanas','El nombre de aduana ya esta registrado.', 'error');</script>")
+                Page.ClientScript.RegisterStartupScript(Me.GetType(), "alert", "<script type=""text/javascript"">swal('Divisas','La divisa ya esta registrado.', 'error');</script>")
             Else
-                Ssql = "INSERT INTO `DB_Nac_Merca`.`tbl_29_Divisas` (`Descripcion`, `Total_Factura`, `Total_Flete`, `Total_Seguro`, `Total_Otros_gastos`) VALUES ('" & txtdescripcion.Text & "', '" & txttotalfactura.Text & "', '" & txttotalflete.Text & "', '" & txttotalseguro.Text & "','" & txttotalotros.Text & "');"
+                Ssql = "INSERT INTO `DB_Nac_Merca`.`tbl_29_Divisas` (`Descripcion`,`Total_Factura`,`Total_Flete`,`Total_Seguro`,`Total_Otros_gastos`) VALUES ('" & txtdescripcion.Text & "','" & txttotalfactura.Text & "','" & txttotalflete.Text & "','" & txttotalseguro.Text & "','" & txttotalotros.Text & "');"
                 Using con As New ControlDB
                     con.GME(Ssql, ControlDB.TipoConexion.Cx_Aduana)
                 End Using
                 Using log_bitacora As New ControlBitacora
-                    log_bitacora.acciones_Comunes(4, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "Se editaron los datos para la aduana con id: " & lblHiddenIDdivisas.Value)
+                    log_bitacora.acciones_Comunes(4, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "Se editaron los datos para la divisa con id: " & lblHiddenIDdivisas.Value)
                 End Using
-                Response.Redirect("~/modulos/mantenimiento/divisas_mant.aspx?acction=editduana")
+                Response.Redirect("~/modulos/mantenimiento/divisas_mant.aspx?acction=editdivisas")
             End If
         Catch ex As Exception
 
@@ -116,14 +116,14 @@
 
     Private Sub bttEliminarDivisa_Click(sender As Object, e As EventArgs) Handles bttEliminarDivisa.Click
         Try
-            Dim Ssql As String = "DELETE FROM `DB_Nac_Merca`.`tbl_06_tbl_29_Divisas` WHERE Id_Divisas= " & lblHiddenIDdivisas.Value
+            Dim Ssql As String = "DELETE FROM `DB_Nac_Merca`.`tbl_29_Divisas` WHERE Id_Divisas= " & lblHiddenIDdivisas.Value
             Using con As New ControlDB
                 con.GME(Ssql, ControlDB.TipoConexion.Cx_Aduana)
             End Using
             Using log_bitacora As New ControlBitacora
                 log_bitacora.acciones_Comunes(6, Session("user_idUsuario"), Session("IDfrmQueIngresa"), "Se elimino la divisa con nombre: " & lblHiddenNombreDivisas.Value & " con exito")
             End Using
-            Response.Redirect("~/modulos/mantenimiento/divisas_mant.aspx?acction=deltedivisa")
+            Response.Redirect("~/modulos/mantenimiento/divisas_mant.aspx?acction=deltedivisas")
         Catch ex As Exception
 
         End Try
