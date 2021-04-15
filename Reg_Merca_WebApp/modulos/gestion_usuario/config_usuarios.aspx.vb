061bdecbb9db4705b08967c028ccfdaedd21fe54
FrancinySauceda <78053289+FrancinySauceda@users.noreply.github.com>
2021-04-15 11:12:43 -0600

    parame

--- Reg_Merca_WebApp/modulos/gestion_usuario/config_usuarios.aspx.vb
+++ Reg_Merca_WebApp/modulos/gestion_usuario/config_usuarios.aspx.vb
@@ -10,15 +10,15 @@
     End Property
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
-        ''parametros de configuracion de sistema
-        'Using Parametros_Sistema As New ControlDB
-        '    Application("ParametrosSYS") = Parametros_Sistema.ParametrosSYS_ADMIN("sistema")
-        'End Using
+        'parametros de configuracion de sistema
+        Using Parametros_Sistema As New ControlDB
+            Application("ParametrosSYS") = Parametros_Sistema.ParametrosSYS_ADMIN("sistema")
+        End Using
 
-        ''PARAMETROS DE ADMINISTRADOR
-        'Using Parametros_admin As New ControlDB
-        '    Application("ParametrosADMIN") = Parametros_admin.ParametrosSYS_ADMIN("adminstrador")
-        'End Using
+        'PARAMETROS DE ADMINISTRADOR
+        Using Parametros_admin As New ControlDB
+            Application("ParametrosADMIN") = Parametros_admin.ParametrosSYS_ADMIN("adminstrador")
+        End Using
         If Session("user_idUsuario") = Nothing Then
             Session.Abandon()
             Response.Redirect("~/Inicio/login.aspx")
