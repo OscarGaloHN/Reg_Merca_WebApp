4b140688eb96a9403354d045f3b2330801399268
SaraRMencias <78059917+SaraRMencias@users.noreply.github.com>
2021-04-19 12:51:39 -0600

    reporte

--- Reg_Merca_WebApp/modulos/declaracion_aduanera/Reporte_caratula.aspx.vb
+++ Reg_Merca_WebApp/modulos/declaracion_aduanera/Reporte_caratula.aspx.vb
@@ -32,7 +32,20 @@ Public Class Reporte_caratula
 
             Dim datasetClientes As New DataSet(Session("DSPoliza"))
             'Get the sales order data  
-            ObtenerDatos(datasetClientes, "DtPoliza", "select Id_poliza, fecha_creacion, Id_cliente, estado_poliza, usuario_creador;")
+            ObtenerDatos(datasetClientes, "DtPoliza", "select a.Id_poliza, a.estado_poliza, a.cod_aduana_ent,  a.cod_aduana_sal, a.Id_regimen, a.nombre_agenciaadu,
+ a.Numero_Preimpreso, a.id_condicion,  a.Id_Clase_deBulto,  a.fecha_creacion, a.divisa_seguro,
+b.nombrec, c.Descripcion, d.nombre, e.nombredecla, f.nombre, g.Nombre_Pais
+
+from tbl_01_polizas a, tbl_04_cliente b, tbl_32_Cod_Documentos c, tbl_02_usuarios d, tbl_43_declarante e, 
+     tbl_05_proveedores f, tbl_8_paises g, tbl_28_Documentos dc
+
+where a.Id_cliente = b.Id_cliente
+and dc.id_poliza_doc = a.Id_poliza
+and dc.Id_Documento = c.Id_Documento
+and a.ultimo_editor = d.id_usuario
+and a.Id_proveedor = f.Id_proveedor
+and a.Cod_pais_org = g.Id_Pais
+and a.declarante = e.id_declarante;")
             Dim dsClientes As New ReportDataSource()
             dsClientes.Name = Session("DSPoliza")
             dsClientes.Value = datasetClientes.Tables(Session("DtPoliza"))
